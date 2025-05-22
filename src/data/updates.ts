568b9e91c2469c5f4d23d4041b3ac3da1618a660
ngs90 <nicolaj.schmit@hotmail.com>
2025-05-22 15:39:54 +0200

    update current job

--- src/data/updates.ts
+++ src/data/updates.ts
@@ -11,17 +11,18 @@ export const updates: Update[] = [
         title: "Code: The GitHub static site is up",
         content: "The base template was created in 10 minutes using https://bolt.new/ including subsites, layout and GitHub deployment pipelines. Adding relevant content and making small adjustments took another hour.",
         link: "https://github.com/ngs90/ngs90.github.io"
-    },
-    {
-        date: "2024-12-13",
-        title: "Article: Databricks free trial learnings",
-        content: "My initial learnings about how to minimize costs using the Databricks free trial on AWS.",
-        link: "https://medium.com/@nicolaj.schmit/databricks-aws-free-trial-learnings-6268d67fca65"
-    },
-    {
-        date: "2024-12-13",
-        title: "Video: Get started with Databricks free trial!",
-        content: "First YouTube tutorial video is out! 😄 I'm still practicing how to make captivating and educational videos. But have a look at how to get started with Databricks on AWS using their free trials.",
-        link: "https://www.youtube.com/watch?v=k4JZflQ4zYM"
     }
+    // ,
+    // {
+    //     date: "2024-12-13",
+    //     title: "Article: Databricks free trial learnings",
+    //     content: "My initial learnings about how to minimize costs using the Databricks free trial on AWS.",
+    //     link: "https://medium.com/@nicolaj.schmit/databricks-aws-free-trial-learnings-6268d67fca65"
+    // },
+    // {
+    //     date: "2024-12-13",
+    //     title: "Video: Get started with Databricks free trial!",
+    //     content: "First YouTube tutorial video is out! 😄 I'm still practicing how to make captivating and educational videos. But have a look at how to get started with Databricks on AWS using their free trials.",
+    //     link: "https://www.youtube.com/watch?v=k4JZflQ4zYM"
+    // }
 ];
